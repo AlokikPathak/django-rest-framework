397391380ae74a5fdc2d48f78ce5979f6a5f39a1
AlokikPathak <alokik2009@gmail.com>
2019-10-02 19:35:41 +0530

    initial commit

---
+++ snippets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from pygments.lexers import get_all_lexers # pygments is used for code highlighting
+from pygments.styles import get_all_styles
+
+
+
+# Create your models here.
